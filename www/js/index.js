e10b42f55964891bcff3c6f05e515552974c609f
majuhbarbarelli <mariajuliabarbareli@hotmail.com>
2020-12-01 13:48:10 -0300

    primeiro commit

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+
+function onDeviceReady() {
+   
+   
+}
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+ 
